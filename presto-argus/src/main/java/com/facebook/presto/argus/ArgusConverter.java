9f70fd042874ef31548c0da7692622d209f44363
David Phillips <david@acz.org>
2013-05-28 15:32:48 -0700

    WIP argus force migrate

--- presto-argus/src/main/java/com/facebook/presto/argus/ArgusConverter.java
+++ presto-argus/src/main/java/com/facebook/presto/argus/ArgusConverter.java
@@ -89,7 +89,7 @@ public class ArgusConverter
             if (validator.resultsMatch()) {
                 migrate = true;
             }
-            else if (validator.getPeregrineState() == PeregrineState.INVALID) {
+            else if (validator.getPeregrineState() != PeregrineState.SUCCESS) {
                 migrate = true;
                 validator.forceQueryTranslation();
             }
